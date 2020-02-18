948c7e8b39cd3c11de01900f246c004aac0ec8f2
SazidAlam <sazidalamanik@gmail.com>
2020-02-19 00:41:39 +0600

    Admin Dashboard Added

---
+++ Admin Dashboard/script.js
@@ -0,0 +1,13 @@
+function saveChange() {
+  alert("Saved Successfully");
+}
+
+function ValidateSize(file) {
+        var FileSize = file.files[0].size / 1024 / 1024; // in MB
+        if (FileSize > 3) {
+            alert('File size exceeds 3 MB');
+           // $(file).val(''); //for clearing with Jquery
+        } else {
+
+        }
+    }
