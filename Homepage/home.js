7f3707e9987c8deee395affa3406e74977f36dd6
coreprinciple97 <ishrat.jahan16@northsouth.edu>
2020-02-19 13:16:56 +0600

    js added to home.html

--- Homepage/home.js
+++ Homepage/home.js
@@ -1,4 +1,12 @@
 function changeColor() {
-    document.getElementsByClassName("sbtn")[0].styles.color = "red";
+    document.getElementsByClassName("sbtn")[0].style.color = "red";
+
+    var newelement = document.createElement("div");
+    newelement.id = "new"
+    var x = "This is the review section"
+    newelement.innerHTML = x;
+
+    document.getElementById("banner").after(newelement);
+ 
   }
   
