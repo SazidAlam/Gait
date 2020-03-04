30f146decc2472f4a88549806e93123d5de0e391
coreprinciple97 <ishrat.jahan16@northsouth.edu>
2020-03-04 15:27:41 +0600

    php files included for session

--- Website/Admin/login.php
+++ Website/Admin/login.php
@@ -1,3 +1,33 @@
+<?php
+   include("connection.php");
+  //  session_start();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      
+      $myusername = mysqli_real_escape_string($conn,$_POST['username']);
+      $mypassword = mysqli_real_escape_string($conn,$_POST['password']); 
+      
+      $sql = "SELECT * FROM info WHERE user='$myusername'";
+      $result = mysqli_query($conn,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      
+      
+      $count = mysqli_num_rows($result);
+      
+      
+        if ($count > 0){
+          if($myusername==$row['username'] && $mypassword==$row['pass']) {
+            header("Location: profile.html");
+        } else {
+            header("Location:login.php?id=username already taken or your password is incorrect. Please try again");
+        }
+        }      
+    
+
+     
+   }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,9 +36,10 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Gait Admin Dashboard</title>
   <link rel="stylesheet" href="css/font-awesome.min.css">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/bootstrap.css">
   <link rel="stylesheet" type="text/css" href="css/style.css">
   <script src="https://kit.fontawesome.com/3ec1573dc2.js" crossorigin="anonymous"></script>
+  <script src="js/script.js"></script>
 </head>
 <body>
   <nav class="navbar navbar-expand-sm navbar-dark bg-dark p-0">
@@ -51,14 +82,14 @@
               <h4>Account Login</h4>
             </div>
             <div class="card-body">
-              <form action="index.html">
+              <form>
                 <div class="form-group">
-                  <label for="email">Email</label>
-                  <input type="text" class="form-control">
+                  <label for="email">User Name</label>
+                  <input type="text" name="username" class="form-control">
                 </div>
                 <div class="form-group">
                   <label for="password">Password</label>
-                  <input type="password" class="form-control">
+                  <input type="password" name="password" class="form-control">
                 </div>
                 <input type="submit" class="btn btn-primary btn-block" value="Login">
               </form>
@@ -69,19 +100,19 @@
     </div>
   </section>
 
-  <footer id="main-footer" class="bg-dark text-white mt-5 p-5">
-    <div class="conatiner">
+  <!-- <footer id="main-footer" class="ab">
+    <div class="">
       <div class="row">
         <div class="col">
           <p class="lead text-center">Copyright &copy; 2020 Gait</p>
         </div>
       </div>
     </div>
-  </footer>
+  </footer> -->
 
-  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
- 
+  <script src="js/jquery.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+
 </body>
 </html>
