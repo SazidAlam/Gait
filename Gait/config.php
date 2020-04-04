972631e1cbabfdf1b0c3f57fbc90ac2916dca9b2
coreprinciple97 <ishrat.jahan16@northsouth.edu>
2020-04-04 20:56:12 +0600

    password for db removed

--- Gait/config.php
+++ Gait/config.php
@@ -1,6 +1,7 @@
 <?php
 
-$conn = mysqli_connect("localhost","root","mysql","gait")
+//$conn = mysqli_connect("localhost","root","mysql","gait")
+$conn = mysqli_connect("localhost","root","","gait")
 			or die("cannot connected");
 
 ?>
