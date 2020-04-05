6e875156e64262f59ba5db38d49706e26e2515c6
coreprinciple97 <ishrat.jahan16@northsouth.edu>
2020-04-05 22:45:01 +0600

    stripe payment implemented

--- Gait/config.php
+++ Gait/config.php
@@ -1,5 +1,16 @@
 <?php
 
+// Product Details 
+// Minimum amount is $0.50 US 
+$itemName = "Demo Product"; 
+$itemNumber = "PN12345"; 
+$itemPrice = 25; 
+$currency = "USD"; 
+ 
+// Stripe API configuration  
+define('STRIPE_API_KEY', 'sk_test_MaFkikwUCJWj7VA60iMhZxky00xfYPndmO'); 
+define('STRIPE_PUBLISHABLE_KEY', 'pk_test_cBtvg262SvdvpaPTbyPOmTYd004Y0FGB1A'); 
+
 //$conn = mysqli_connect("localhost","root","mysql","gait")
 $conn = mysqli_connect("localhost","root","","gait")
 			or die("cannot connected");
